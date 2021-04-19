d34b76be3a98980fe184673687209a4e13aeaa2d
Bilal Nurhusien <BNurhusien@sierrawireless.com>
2021-04-18 22:36:40 -0700

    Update model

--- LTEProject.py
+++ LTEProject.py
@@ -7,7 +7,7 @@ import os
 import shap
 import tensorflow as tf
 import numpy as np
-
+import sys
 from tensorflow import keras
 from datetime import datetime
 from pandas import DataFrame
@@ -23,6 +23,7 @@ from sklearn.model_selection import train_test_split
 from keras.preprocessing.sequence import TimeseriesGenerator
 from IPython.display import Image, display
 from sklearn.preprocessing import QuantileTransformer
+from scipy.ndimage.interpolation import shift
 
 # Constants
 DATASET_FOLDER_PATH=r'D:\ENSC813\Training\Training\LTE_Dataset\Dataset'
@@ -44,7 +45,7 @@ WINDOW_LENGTH=10
 BATCH_SIZE=32
 STRIDE=1
 LSTM_NODES=100
-EPOCHS=50
+EPOCHS=200
 SAMPLING_RATE=1
 TEST_RATIO=0.2
 
@@ -63,6 +64,7 @@ def create_dataset():
 	column_names.remove(DOWNLOAD_BITRATE_KEY)
 	column_names = [DOWNLOAD_BITRATE_KEY] + column_names
 	dataset=dataset.reindex(columns=column_names)
+	dataset = dataset[(dataset[DOWNLOAD_BITRATE_KEY] > 0 )]
 	print(dataset.head())
 
 	# Drop columns that are repetitive or unneeded
@@ -73,7 +75,7 @@ def create_dataset():
 	print(dataset.head())
 	print(dataset.describe())
 
-#create_dataset()
+create_dataset()
 df = pd.read_csv(OUTPUT_FILE_PATH, header=0, index_col=0, na_values=NA_VALUES, parse_dates=[TIMESTAMP_KEY])
 print(df)
 print(df.info())
@@ -81,6 +83,8 @@ print(df.count())
 print(df.describe())
 df = df.fillna(method='ffill')
 
+start_index = 0
+end_index=df.shape[0]
 quantile = MinMaxScaler(feature_range=(0,1))
 # scale data between 0 and 1
 data_transform = quantile.fit_transform(df)
@@ -88,9 +92,13 @@ features_scaled=data_transform
 target_scaled=data_transform[:,0]
 x_train, x_test, y_train, y_test = train_test_split(features_scaled, target_scaled, test_size=TEST_RATIO, random_state=1, shuffle=False)
 
+multi_target_train = shift(y_train, 0, cval=np.NaN)
+multi_target_test = shift(y_test, 0, cval=np.NaN)
+#multi_target_train = [shift(y_train, -12, cval=np.NaN), shift(y_train, -16, cval=np.NaN), shift(y_train, -32, cval=np.NaN)]
+#multi_target_test = [shift(y_test, -12, cval=np.NaN), shift(y_test, -16, cval=np.NaN), shift(y_test, -32, cval=np.NaN)]
 num_of_features=len(df.columns)
-train_generator = TimeseriesGenerator(x_train, y_train, length=WINDOW_LENGTH, stride=STRIDE, sampling_rate=SAMPLING_RATE, batch_size=BATCH_SIZE)
-test_generator = TimeseriesGenerator(x_test, y_test, length=WINDOW_LENGTH, sampling_rate=SAMPLING_RATE, batch_size=BATCH_SIZE)
+train_generator = TimeseriesGenerator(x_train, multi_target_train, length=WINDOW_LENGTH, stride=STRIDE, sampling_rate=SAMPLING_RATE, batch_size=BATCH_SIZE)
+test_generator = TimeseriesGenerator(x_test, multi_target_test, length=WINDOW_LENGTH, sampling_rate=SAMPLING_RATE, batch_size=BATCH_SIZE)
 
 print(train_generator[0])
 model = Sequential()
@@ -104,9 +112,9 @@ early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
 
 cb_list = [early_stopping]
 
-model.compile(loss=tf.losses.MeanSquaredError(),
+model.compile(loss=tf.losses.MeanAbsoluteError(),
 		      optimizer=tf.optimizers.Adam(),
-		      metrics=[tf.metrics.MeanAbsolutePercentageError()])
+		      metrics=[tf.metrics.MeanSquaredError()])
 
 history = model.fit(train_generator, epochs=EPOCHS, validation_data=test_generator, callbacks=cb_list, verbose=2, shuffle=False)
 model.evaluate(test_generator, verbose=0)
@@ -119,16 +127,16 @@ predictions_train = model.predict(train_generator)
 df_prediction = pd.concat([pd.DataFrame(predictions), pd.DataFrame(x_test[:,1:][WINDOW_LENGTH:])], axis=1)
 df_prediction_train = pd.concat([pd.DataFrame(predictions_train), pd.DataFrame(x_train[:,1:][WINDOW_LENGTH:])], axis=1)
 
-
 rev_trans_test_pred=quantile.inverse_transform(df_prediction)
 rev_trans_train_pred=quantile.inverse_transform(df_prediction_train)
-
-df_final = df[predictions.shape[0]*-1:]
+y_true, y_pred = df[DOWNLOAD_BITRATE_KEY][rev_trans_test_pred.shape[0]  * -1:],  rev_trans_test_pred[:,0]
+yerror_mean = mean_absolute_percentage_error(y_true, y_pred)
+yerror_std = np.std(yerror_mean)
 
 # Display ground truth and predictions on plot
 x_data = range(df.shape[0])
-plt.plot(x_data[rev_trans_train_pred.shape[0] + 2*WINDOW_LENGTH:], df[DOWNLOAD_BITRATE_KEY][rev_trans_train_pred.shape[0] + 2*WINDOW_LENGTH:], label='ground truth')
-plt.plot(x_data[rev_trans_train_pred.shape[0] + 2*WINDOW_LENGTH:], rev_trans_test_pred[:,0],  color='r', label='test prediction')
+plt.plot(x_data[-rev_trans_test_pred.shape[0]:], df[DOWNLOAD_BITRATE_KEY][-rev_trans_test_pred.shape[0]:], label='ground truth')
+plt.plot(x_data[-rev_trans_test_pred.shape[0]:], rev_trans_test_pred[:,0],  color='r', label='test prediction')
 plt.show()
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='test')
@@ -136,9 +144,6 @@ plt.xlabel("Epochs")
 plt.ylabel("Mean Squared Error")
 plt.legend(loc='upper right')
 plt.show()
-yerror = mean_absolute_percentage_error(df[DOWNLOAD_BITRATE_KEY][rev_trans_train_pred.shape[0] + 2*WINDOW_LENGTH:], rev_trans_test_pred[:,0])
-yerror_mean = np.mean(yerror)
-yerror_std = np.std(yerror)
 
 # Build the plot
 fig, ax = plt.subplots()
